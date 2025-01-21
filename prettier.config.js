bc8a98093df52c369ee66bed7c4ecc7e960805a8
MarcelOlsen <marcelolsen26@gmail.com>
2025-01-21 01:03:22 +0100

    Env setup

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  useTabs: true,
+  trailingComma: "es5",
+  tabWidth: 4,
+};
+
+export default config;
